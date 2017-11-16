63aaf7fc329ad28a755891964fb15a6f7e4d1169
Douglas Moura <douglasmourab91@gmail.com>
2017-11-16 19:48:10 -0200

    Atualizando arquivos do projeto

---
+++ src/com/algaworks/cursojsf2/financeiro/service/RegraNegocioException.java
@@ -0,0 +1,11 @@
+package com.algaworks.cursojsf2.financeiro.service;
+
+public class RegraNegocioException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public RegraNegocioException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/com/algaworks/cursojsf2/financeiro/util/Repositorios.java
@@ -0,0 +1,28 @@
+package com.algaworks.cursojsf2.financeiro.util;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+
+import com.algaworks.cursojsf2.financeiro.repository.Lancamentos;
+import com.algaworks.cursojsf2.financeiro.repository.Pessoas;
+import com.algaworks.cursojsf2.financeiro.repository.infra.LancamentosHibernate;
+import com.algaworks.cursojsf2.financeiro.repository.infra.PessoasHibernate;
+
+public class Repositorios implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Pessoas getPessoas() {
+		return new PessoasHibernate(this.getSession());
+	}
+	
+	public Lancamentos getLancamentos() {
+		return new LancamentosHibernate(this.getSession());
+	}
+	
+	private Session getSession() {
+		return (Session) FacesUtil.getRequestAttribute("session");
+	}
+	
+}
